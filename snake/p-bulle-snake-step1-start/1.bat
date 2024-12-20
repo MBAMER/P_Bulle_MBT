003df418130850fe6c36741a56f258d789ffb9e9
MBAMER <163824331+MBAMER@users.noreply.github.com>
2024-12-20 14:44:47 +0100

    Docs(secret santa)Emballage cadeau
    [15][done]
    Emballage cadeau + mis des cadeau dans la salle N505

--- snake/p-bulle-snake-step1-start/1.bat
+++ snake/p-bulle-snake-step1-start/1.bat
@@ -1,5 +1,4 @@
 @echo off
-0%|0%
 start cmd /k "npm run dev"
 timeout /t 3 > nul
-start C:/Users/matBamert/Documents/GitHub/P_Bulle_MBT/snake/p-bulle-snake-step1-start/index.html
+start http://localhost:3000/
