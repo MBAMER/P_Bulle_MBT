1125cf875b95cdc087760f6299c5ff2dee33b3b4
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-01-10 16:03:12 +0100

    feat(main)Rajou du timer
    [30][done]
    Rajout d'un timer pour compter le nombre de seconde

--- snake/p-bulle-snake-step1-start/src/main.js
+++ snake/p-bulle-snake-step1-start/src/main.js
@@ -10,8 +10,10 @@ const ctx = canvas.getContext("2d");
 const box = 20;
 const gameSpeed = 200;
 let snake;
+let Timer = 0;
+let timerInterval;
 window.food = generateFood(box, canvas);
-let direction = "DOWN";
+let direction = "RIGHT";
 window.score = 0;
 let gameInterval; // Variable pour stocker l'identifiant de l'intervalle
 
@@ -20,27 +22,50 @@ document.addEventListener("keydown", (event) => {
 });
 
 function startGame() {
+  snake = initSnake();
+  direction = "RIGHT";
+  Timer = 0;
+  score = 0;
+  replaybutton.style.display = "none";
   gameInterval = setInterval(() => {
     update();
     draw();
   }, gameSpeed); // Stockage de l'identifiant de l'intervalle
-  snake = initSnake();
+  timerInterval = setInterval(updateTimer, 1000); // Démarre le timer
+}
+
+function updateTimer() {
+  Timer++;
 }
 
 function update() {
   let head = snake[0];
   moveSnake(snake, direction, box, canvas);
-  if (checkWallCollision(snake.at(0), canvas, box)) alert("gameover");
-  if (checkCollision(snake)) alert("gameover");
-}
+  if (checkWallCollision(snake.at(0), canvas, box)) {
+    clearInterval(gameInterval);
+    clearInterval(timerInterval); // Arrête le timer
+    replaybutton.style.display = "block";
+  }
 
+  if (checkCollision(snake)) {
+    clearInterval(gameInterval);
+    clearInterval(timerInterval); // Arrête le timer
+    replaybutton.style.display = "block";
+  }
+}
 function draw() {
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
   drawFood(ctx, food, box);
   drawSnake(ctx, snake, box);
   drawScore(ctx, score);
 
-  // A compléter
+  // Affiche le timer
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "black";
+  ctx.fillText("Time: " + Timer + "s", canvas.clientWidth - 100, 20);
 }
+replaybutton.addEventListener("click", () => {
+  startGame();
+});
 
 startGame();
